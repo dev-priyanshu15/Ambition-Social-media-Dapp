c68bbd5d4f91792cc7edb3be6879a8df57bc2cfa
Priyanshu Singh <123263608+dev-priyanshu15@users.noreply.github.com>
2024-12-08 01:01:19 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,5 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
 }
-
+module.exports = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"]
+    });
+    return config;
+  }
+}
 module.exports = nextConfig
